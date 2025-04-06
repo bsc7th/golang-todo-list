9d963a6809dcf58110186ddac9c38d3ee6990c32
bsc7th <ryanjayleyva@proton.me>
2025-04-06 02:19:28 -0400

    Add new variables

--- main.go
+++ main.go
@@ -8,9 +8,20 @@ func main() {
 	var taskOneB = "Setup Neovim and install Go via Mason"
 	var taskOneC = "Verify if everything is working"
 
-	var taskTwo = "Create a Simple Todo App"
-	var taskTwoA = "Create a Directory for my project by running 'mkdir go-todo-app' in the terminal"
-	var taskTwoB = "Navigate to go-todo-app by running z go-todo-app" // I install zoxide that's why I'm using the command z instead of cd
+	var taskTwo = "Learn Go Syntax"
+	var taskTwoA = "Understand variables, data types, and constants"
+	var taskTwoB = "Learn how to declare  and use functions"
+	var taskTwoC = "Get familiar with control structure (if, switch for loops)"
+
+	var taskThree = "Create a Simple Todo App"
+	var taskThreeTA = "Create a Directory for my project by running 'mkdir go-todo-app' in the terminal"
+	var taskThreeB = "Navigate to go-todo-app by running z go-todo-app" // I install zoxide that's why I'm using the command z instead of cd
+	var taskThreeC = "Initialize the Go Module by running this command 'go mod init go-todo-app'"
+	var taskThreeD = "Create a Go File by running 'touch main.go'"
+	var taskThreeE = "Import package main"
+	var taskThreeF = "Import 'fmt'"
+	var taskThreeG = "Start writing Go code"
+	var taskThreeH = "Run the Go Program 'go run main.go'"
 
 	fmt.Println("###### Golang Learning To-Dos: ######")
 
@@ -19,13 +30,19 @@ func main() {
 	fmt.Println(taskOneB)
 	fmt.Println(taskOneC)
 
-	fmt.Println()  // new line
-	fmt.Println(taskTwo)
-	fmt.Println(taskTwoA)
-	fmt.Println(taskTwoB)
-
 	fmt.Println() // new line
 	fmt.Println("Rewards")
 	fmt.Println("Reward myself with a cheesecake")
+
+	fmt.Println()  // new line
+	fmt.Println(taskThree)
+	fmt.Println(taskThreeA)
+	fmt.Println(taskThreeB)
+	fmt.Println(taskThreeC)
+	fmt.Println(taskThreeD)
+	fmt.Println(taskThreeE)
+	fmt.Println(taskThreeF)
+	fmt.Println(taskThreeG)
+	fmt.Println(taskThreeH)
 }
 
