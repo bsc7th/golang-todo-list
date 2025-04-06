093b476e9a95bfd939858d1f74b52f8d006bdf11
bsc7th <ryanjayleyva@proton.me>
2025-04-06 02:26:46 -0400

    Add new task variables

--- main.go
+++ main.go
@@ -12,9 +12,18 @@ func main() {
 	var taskTwoA = "Understand variables, data types, and constants"
 	var taskTwoB = "Learn how to declare  and use functions"
 	var taskTwoC = "Get familiar with control structure (if, switch for loops)"
+	var taskTwoD = "Explore Go's Data Structure"
+	var taskTwoE = "Dive into Go' Concurrency Model"
+	var taskTwoF = "Understand Error Handling in Go"
+	var taskTwoG = "Explore Go's Standard Library"
+	var taskTwoH = "Practice Writing Unit Tests in Go"
+	var taskTwoI = "Learn About Go Modules and Dependency Management"
+	var taskTwoJ = "Understand Go’s Memory Management"
+	var taskTwoK = "Learn About Go’s Web Frameworks"
+	var taskTwoL = "Learn About Go’s Performance Optimization"
 
 	var taskThree = "Create a Simple Todo App"
-	var taskThreeTA = "Create a Directory for my project by running 'mkdir go-todo-app' in the terminal"
+	var taskThreeA = "Create a Directory for my project by running 'mkdir go-todo-app' in the terminal"
 	var taskThreeB = "Navigate to go-todo-app by running z go-todo-app" // I install zoxide that's why I'm using the command z instead of cd
 	var taskThreeC = "Initialize the Go Module by running this command 'go mod init go-todo-app'"
 	var taskThreeD = "Create a Go File by running 'touch main.go'"
@@ -31,8 +40,10 @@ func main() {
 	fmt.Println(taskOneC)
 
 	fmt.Println() // new line
-	fmt.Println("Rewards")
-	fmt.Println("Reward myself with a cheesecake")
+	fmt.Println(taskTwo)
+	fmt.Println(taskTwoA)
+	fmt.Println(taskTwoB)
+	fmt.Println(taskTwoC)
 
 	fmt.Println()  // new line
 	fmt.Println(taskThree)
